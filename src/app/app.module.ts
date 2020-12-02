79b43cacd289825d7521ece37a17084217012e9c
Lynnette25 <frances.heredia@gmail.com>
2020-12-02 07:56:08 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent , HeaderComponent ],
+  declarations: [ AppComponent , HeaderComponent, RecipesComponent,  
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
