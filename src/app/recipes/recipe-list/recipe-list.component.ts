e4f6cf7196cc61ecc3ed1c2132fbe82c3217a7c0
Lynnette25 <frances.heredia@gmail.com>
2020-12-03 06:56:14 -0400

    1 file modified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 
-export class RecipeListComponent{
+export class RecipeListComponent implements OnInit {
 
+ recipes = [];
 
+  constructor() {}
+  ngOnInit(){}
 }
