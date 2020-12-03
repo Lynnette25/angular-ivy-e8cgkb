fb1a249a1010c57032cf4cbc1252cbf8f5f9c3dc
Lynnette25 <frances.heredia@gmail.com>
2020-12-03 07:27:40 -0400

    4 files modified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 
+
+import { Recipe } from '../recipes.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
@@ -7,8 +10,12 @@ import { Component, OnInit} from '@angular/core';
 
 export class RecipeListComponent implements OnInit {
 
- recipes = [];
+ recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
   constructor() {}
   ngOnInit(){}
+  
 }
